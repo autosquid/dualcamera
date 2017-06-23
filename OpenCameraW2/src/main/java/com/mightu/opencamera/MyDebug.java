20b718a3b1d7566bd112e42508d0580bb8610ad6
w.z <justin.seeley.cn@gmail.com>
2017-06-23 11:40:00 +0800

    switch camera between two rear cameras

---
+++ OpenCameraW2/src/main/java/com/mightu/opencamera/MyDebug.java
@@ -0,0 +1,8 @@
+package com.mightu.opencamera;
+
+/** Global constant to control logging, should always be set to false in
+ *  released versions.
+ */
+public class MyDebug {
+	public static final boolean LOG = true;
+}
